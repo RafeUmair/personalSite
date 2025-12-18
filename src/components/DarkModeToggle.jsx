8ac0e19e7c08f40577a04d8440f118d36502a886
Rafe Umair <raferaed98@gmail.com>
2025-12-18 14:23:11 +0800

    Added basic styling and started work on dark mode toggle

---
+++ src/components/DarkModeToggle.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Sun, Moon } from "lucide-react";
+
+export const DarkModeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <button onClick={toggleDarkMode}>
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
+    </button>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { DarkModeToggle } from "../components/DarkModeToggle";
+
+export const Home = () => {
+  return( 
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
+      {/* Dark Mode Toggle */}
+      <DarkModeToggle />
+
+      {/* Background Animation */}
+
+      {/* Menu Bar */}
+
+      {/* Main Content */}
+      <h1 className="text-4xl font-bold">Home Page</h1>
+
+      {/* Footer */}
+    </div>
+  );
+};
